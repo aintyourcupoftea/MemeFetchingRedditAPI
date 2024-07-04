d55e709812c3109a9a3c029bb44c538e50bfecc9
Amit Gavali <shambho@Amits-MacBook-Air.local>
2024-07-05 03:41:03 +0530

    🔥 fixed port in main.py

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ def index():
     return render_template('index.html', url=cached_post)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 7860)))
