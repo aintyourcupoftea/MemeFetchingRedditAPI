e01afb95d014be172e89982eceaa2b255eaee98c
Amit Gavali <shambho@Amits-MacBook-Air.local>
2024-07-05 04:00:52 +0530

    🔥 ata image direct disnar

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, send_file
 import praw
 import os
+import requests
+from io import BytesIO
 import time
 
-
 app = Flask(__name__)
-
-# Variables to store cached data
-cached_post = None
-cached_time = 0
 
 # Reddit API credentials
 reddit = praw.Reddit(
@@ -17,25 +14,34 @@ reddit = praw.Reddit(
     user_agent='GithubReadMe'
 )
 
-# Function to fetch top post from r/ProgrammerHumor
-def fetch_top_post():
-    subreddit = reddit.subreddit('ProgrammerHumor')
-    top_post = next(subreddit.top('day', limit=1))
-    return top_post.url
+# Variables to store cached data
+cached_post_url = None
+cached_post_image = None
+cached_time = 0
 
-# Route to display the top post image
+# Function to fetch top post URL and image from r/ProgrammerHumor
+def fetch_top_post():
+    global cached_post_url, cached_post_image, cached_time
+    
+    subreddit = reddit.subreddit('ProgrammerHumor')
+    top_post = next(subreddit.top('day', limit=1))  # Fetch top post of the day
+    
+    cached_post_url = top_post.url
+    cached_post_image = requests.get(cached_post_url)
+    cached_time = time.time()
+    
+    return cached_post_image
+
 @app.route('/')
 def index():
-    global cached_post, cached_time
+    global cached_post_image, cached_time
 
-    # Check if cached data exists and is still valid (within 24 hours)
     current_time = time.time()
-    if cached_post is None or current_time - cached_time > 86400:
+    if cached_post_image is None or current_time - cached_time > 86400:
         # Fetch new data from Reddit API
-        cached_post = fetch_top_post()
-        cached_time = current_time
+        fetch_top_post()
 
-    return render_template('index.html', url=cached_post)
+    return send_file(BytesIO(cached_post_image.content), mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 7860)))
